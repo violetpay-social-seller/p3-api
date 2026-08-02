1e80338eb7a7399a9dbe4b77151d536bd2bf3ddc
kmsdevdata-sketch <kms.dev.data@gmail.com>
2026-08-02 16:05:38 +0900

    feat:exception-BaseException

---
+++ src/main/java/io/point3/p3api/exception/BaseException.java
@@ -0,0 +1,18 @@
+package io.point3.p3api.exception;
+
+import lombok.Getter;
+
+@Getter
+public class BaseException extends RuntimeException {
+    private final ErrorCode errorCode;
+
+    public BaseException(ErrorCode errorCode) {
+        super(errorCode.getTitle());
+        this.errorCode = errorCode;
+    }
+
+    public BaseException(ErrorCode errorCode, String detail) {
+        super(detail);
+        this.errorCode = errorCode;
+    }
+}

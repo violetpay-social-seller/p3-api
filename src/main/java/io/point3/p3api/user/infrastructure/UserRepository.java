ea54c611acb3bc56b61ef8277ff2c6749bdb19ab
kmsdevdata-sketch <kms.dev.data@gmail.com>
2026-08-03 11:48:10 +0900

    feat:user-port/infrastructure

---
+++ src/main/java/io/point3/p3api/user/infrastructure/UserRepository.java
@@ -0,0 +1,14 @@
+package io.point3.p3api.user.infrastructure;
+
+import io.point3.p3api.user.domain.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+    Optional<User> findByCognitoSub(String cognitoSub);
+}

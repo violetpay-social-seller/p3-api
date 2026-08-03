bfc6a13901bad01f0f5eed88d001739570492a78
kmsdevdata-sketch <kms.dev.data@gmail.com>
2026-08-03 14:06:45 +0900

    feat:custom-annotation-authenticated

---
+++ src/main/java/io/point3/p3api/auth/infrastructure/web/Authenticated.java
@@ -0,0 +1,11 @@
+package io.point3.p3api.auth.infrastructure.web;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Authenticated {
+}

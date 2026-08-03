09e76bd693d8d5e0d14248d34f172989253de2c6
kmsdevdata-sketch <kms.dev.data@gmail.com>
2026-08-03 11:50:27 +0900

    feat:user-UserPersistenceAdapter

--- src/main/java/io/point3/p3api/user/infrastructure/UserJpaRepository.java
+++ src/main/java/io/point3/p3api/user/infrastructure/UserJpaRepository.java
@@ -8,7 +8,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, UUID> {
+public interface UserJpaRepository extends JpaRepository<User, UUID> {
 
     Optional<User> findByCognitoSub(String cognitoSub);
 }
---
+++ src/main/java/io/point3/p3api/user/infrastructure/UserPersistenceAdapter.java
@@ -0,0 +1,20 @@
+package io.point3.p3api.user.infrastructure;
+
+import io.point3.p3api.user.application.port.UserRender;
+import io.point3.p3api.user.domain.entity.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+@RequiredArgsConstructor
+public class UserPersistenceAdapter implements UserRender {
+
+    private final UserJpaRepository userJpaRepository;
+
+    @Override
+    public Optional<User> findByCognitoSub(String cognitoSub) {
+        return userJpaRepository.findByCognitoSub(cognitoSub);
+    }
+}

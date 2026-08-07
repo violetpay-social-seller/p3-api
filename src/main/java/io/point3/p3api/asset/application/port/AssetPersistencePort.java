8ce82d599ee9c8bb2197b281984f9f52d88fb2ef
kmsdevdata-sketch <kms.dev.data@gmail.com>
2026-08-07 14:54:34 +0900

    feat:asset store&save

---
+++ src/main/java/io/point3/p3api/asset/application/port/AssetPersistencePort.java
@@ -0,0 +1,4 @@
+package io.point3.p3api.asset.application.port;
+
+public interface AssetPersistencePort {
+}
---
+++ src/main/java/io/point3/p3api/asset/infrastructure/s3/AssetStoreRequest.java
@@ -0,0 +1,4 @@
+package io.point3.p3api.asset.infrastructure.s3;
+
+public class AssetStoreRequest {
+}

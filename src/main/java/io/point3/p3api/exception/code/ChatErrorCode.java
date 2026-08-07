ea31d19789f7961f5f3f887ad7cab06e17786df1
ejinn1 <ejinn1@naver.com>
2026-08-07 17:09:38 +0900

    feat: 채팅 메시지 저장 유스케이스 추가

---
+++ src/main/java/io/point3/p3api/exception/code/ChatErrorCode.java
@@ -0,0 +1,26 @@
+package io.point3.p3api.exception.code;
+
+import io.point3.p3api.exception.ErrorCode;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public enum ChatErrorCode implements ErrorCode {
+    CHAT_PARTICIPANT_FORBIDDEN(
+            "CHAT_PARTICIPANT_FORBIDDEN_403",
+            "Chat room participant required",
+            HttpStatus.FORBIDDEN,
+            "/errors/chat/participant-forbidden");
+
+    private final String code;
+    private final String title;
+    private final HttpStatus status;
+    private final String type;
+
+    ChatErrorCode(String code, String title, HttpStatus status, String type) {
+        this.code = code;
+        this.title = title;
+        this.status = status;
+        this.type = type;
+    }
+}

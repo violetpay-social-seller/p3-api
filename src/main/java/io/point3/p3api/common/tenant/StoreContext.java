02652422bde649a3ccfe0ef3477cda386b7a186b
kmsdevdata-sketch <kms.dev.data@gmail.com>
2026-08-12 15:30:03 +0900

    refactor:tenant-provider-filter

---
+++ src/main/java/io/point3/p3api/common/tenant/StoreContext.java
@@ -0,0 +1,42 @@
+package io.point3.p3api.common.tenant;
+
+import io.point3.p3api.exception.BaseException;
+import io.point3.p3api.exception.code.CommonErrorCode;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+/**
+ *  결정된 storeId를 요청 스코프내에서 꺼내쓰는 역할
+ */
+@NoArgsConstructor
+public final class StoreContext {
+
+  private static final ScopedValue<UUID> STORE_ID = ScopedValue.newInstance();
+
+  public static ScopedValue.Carrier where(UUID storeId) {
+    if (storeId == null) {
+      throw new BaseException(CommonErrorCode.INVALID_INPUT);
+    }
+
+    return ScopedValue.where(STORE_ID, storeId);
+  }
+
+  public static UUID getStoreId() {
+    return STORE_ID.isBound() ? STORE_ID.get() : null;
+  }
+
+  public static UUID getRequiredStoreId() {
+    UUID storeId = getStoreId();
+
+    if (storeId == null) {
+      throw new BaseException(CommonErrorCode.INVALID_INPUT);
+    }
+
+    return storeId;
+  }
+
+  public static boolean hasStore() {
+    return STORE_ID.isBound() && STORE_ID.get() != null;
+  }
+}
--- src/main/java/io/point3/p3api/common/tenant/resolvers/TenantResolver.java
+++
@@ -1,9 +0,0 @@
-package io.point3.p3api.common.tenant.resolvers;
-
-import java.util.Optional;
-
-@FunctionalInterface
-public interface TenantResolver<T> {
-
-  Optional<String> resolveTenantIdentifier(T source);
-}

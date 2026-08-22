d0dd71b7c3e78b7201fd974c12be59a890b9805e
kmsdevdata-sketch <kms.dev.data@gmail.com>
2026-08-23 01:20:38 +0900

    feaT:필터추가

--- src/main/java/io/point3/p3api/inquiry/application/list/InquiryListUseCase.java
+++ src/main/java/io/point3/p3api/inquiry/application/list/InquiryListUseCase.java
@@ -1,13 +1,16 @@
 package io.point3.p3api.inquiry.application.list;
 
 import io.point3.p3api.inquiry.application.result.InquiryListItem;
+import io.point3.p3api.inquiry.domain.type.InquiryStatus;
+
 import java.util.List;
 import java.util.UUID;
 
 public interface InquiryListUseCase {
-  List<InquiryListItem> getBuyerInquiries(UUID buyerUserId);
+  List<InquiryListItem> getBuyerInquiries(UUID buyerUserId, InquiryStatus status);
 
-  List<InquiryListItem> getSellerInquiries(UUID storeId, UUID sellerUserId);
+  List<InquiryListItem> getSellerInquiries(UUID storeId, UUID sellerUserId, InquiryStatus status);
+
 
   void markBuyerRead(UUID inquiryId, UUID buyerUserId);
 
